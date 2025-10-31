cd6820cd0263b9814d216fa860cbaf07992a6b2d
miro <jarbasai@mailfence.com>
2025-10-31 07:01:24 +0000

    fix

--- ovos_ww_plugin_precise_onnx/__init__.py
+++ ovos_ww_plugin_precise_onnx/__init__.py
@@ -20,34 +20,13 @@ def vectorize_audio(audio, n_mfcc, n_features, rate, hop_length):
     return mfcc
 
 
-class TriggerDetector:
-    def __init__(self, chunk_size, sensitivity=0.5, trigger_level=3):
-        self.chunk_size = chunk_size
-        self.sensitivity = sensitivity
-        self.trigger_level = trigger_level
-        self.activation = 0
-
-    def update(self, prob):
-        chunk_activated = prob > 1.0 - self.sensitivity
-        if chunk_activated or self.activation < 0:
-            self.activation += 1
-            has_activated = self.activation > self.trigger_level
-            if has_activated or (chunk_activated and self.activation < 0):
-                self.activation = -(8 * 2048) // self.chunk_size
-            if has_activated:
-                return True
-        elif self.activation > 0:
-            self.activation -= 1
-        return False
-
-
 class PreciseOnnxHotwordPlugin(HotWordEngine):
 
     def __init__(self, key_phrase="hey mycroft", config=None):
         super().__init__(key_phrase, config)
-        self.has_found = False
 
-        self.trigger_level = self.config.get('trigger_level', 3)
+        self.activations = 0
+        self.trigger_level = self.config.get('trigger_level', 1)
         self.sensitivity = self.config.get('sensitivity', 0.5)
 
         default_model = "https://github.com/OpenVoiceOS/precise-onnx-models/raw/master/wakewords/en/hey_mycroft.onnx"
@@ -74,8 +53,6 @@ class PreciseOnnxHotwordPlugin(HotWordEngine):
         self.n_mfcc = int(input_shape[2])
 
         self.buffer = np.zeros(self.sample_rate, dtype=np.float32)
-        self.detector = TriggerDetector(self.hop_length, self.sensitivity, self.trigger_level)
-
 
     @staticmethod
     def download_model(url):
@@ -92,24 +69,24 @@ class PreciseOnnxHotwordPlugin(HotWordEngine):
             LOG.info(f"Model downloaded to {model_path}")
         return model_path
 
-    def on_activation(self):
-        self.has_found = True
-
     def update(self, chunk):
-        chunk = np.frombuffer(chunk, dtype=np.int16).astype(np.float32) / 32768.0
-        self.buffer = np.roll(self.buffer, -self.hop_length)
-        self.buffer[-self.hop_length:] = chunk
+        audio = np.frombuffer(chunk, dtype=np.int16).astype(np.float32) / 32768.0
+        for start in range(0, len(audio), self.hop_length):
+            frame = audio[start:start + self.hop_length]
+            if len(frame) < self.hop_length:
+                continue
+            self.buffer = np.roll(self.buffer, -self.hop_length)
+            self.buffer[-self.hop_length:] = frame
 
-        features = vectorize_audio(self.buffer, self.n_mfcc, self.n_features, self.sample_rate, self.hop_length)
-        inp = features[np.newaxis, :, :].astype(np.float32)
-
-        out = self.session.run(None, {self.input_name: inp})[0][0][0]
-        self.has_found = self.detector.update(out)
+            features = vectorize_audio(self.buffer, self.n_mfcc, self.n_features,
+                                       self.sample_rate, self.hop_length)
+            inp = features[np.newaxis, :, :].astype(np.float32)
+            out = self.session.run(None, {self.input_name: inp})[0][0][0]
+            if out > 0:
+                self.activations += 1
 
     def found_wake_word(self):
-        if self.has_found:
-            self.has_found = False
-            self.detector.activation = 0 # reset counter
+        if self.activations >= self.trigger_level:
+            self.activations = 0  # reset counter
             return True
         return False
-
